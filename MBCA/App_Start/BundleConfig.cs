e8476dbda56e10b8f3913a7e802c23ee7c22af78
Syahrul Setiawan <kiphe19@gmail.com>
2016-01-06 20:53:26 +0700

    Nightly Update

--- MBCA/App_Start/BundleConfig.cs
+++ MBCA/App_Start/BundleConfig.cs
@@ -11,8 +11,7 @@ namespace chevron
                     "~/DataTables/datatables.min.js",
                     "~/Scripts/bootstrap.min.js",
                     "~/Scripts/dataTables.editor.min.js",
-                    "~/Scripts/moment.min.js",
-                    "~/Scripts/_main.js"
+                    "~/Scripts/moment.min.js"
                 ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
@@ -20,6 +19,11 @@ namespace chevron
                     "~/content/editor.dataTables.min.css",
                     "~/content/metro-bootstrap.min.css"
                 ));
+
+            bundles.Add(new ScriptBundle("~/Script/xdkJKAds").Include("~/Scripts/_main.js"));
+            bundles.Add(new ScriptBundle("~/Script/cfgxYSa").Include("~/Scripts/_config.js"));
+
+
             BundleTable.EnableOptimizations = true;
         }
     }
