e355964628c0b1f419e6d7603905ea22f7a9e7df
jono <joejond@gmail.com>
2016-04-04 11:50:13 +0700

    benerin data tabel report

--- MBCA/Scripts/_report.js
+++ MBCA/Scripts/_report.js
@@ -4,51 +4,51 @@
 //var tablee = tablee = $("table").DataTable({ "scrollX": true });
 $("#DataTables_Table_0_wrapper").hide();
 
-function loadTableData(data) {
-    //console.log(data);
-    var buka = '<tr>';
-    var tutup = '</tr>';
-    $("table").html(null);
-    tablee.destroy();
+//function loadTableData(data) {
+//    //console.log(data);
+//    var buka = '<tr>';
+//    var tutup = '</tr>';
+//    $("table").html(null);
+//    tablee.destroy();
 
-    $.post(path + '/Reporting', data)
-        .done(function (res) {
-            //$("table").html(aseliTable);
-            var a = res;
+//    $.post(path + '/Reporting', data)
+//        .done(function (res) {
+//            //$("table").html(aseliTable);
+//            var a = res;
 
-            var countUnit = 0;
+//            var countUnit = 0;
 
-            for (var index in a.unit) {
-                countUnit++;
-                //$("table thead tr").append('<th>' + a.unit[index] + '</th>');
-            }
+//            for (var index in a.unit) {
+//                countUnit++;
+//                //$("table thead tr").append('<th>' + a.unit[index] + '</th>');
+//            }
 
-            for (var index in a.data) {
-                var isi = '<td>' + a.data[index].tgl + '</td>' +
-                '<td>' + a.data[index].vessel + '</td>';
+//            for (var index in a.data) {
+//                var isi = '<td>' + a.data[index].tgl + '</td>' +
+//                '<td>' + a.data[index].vessel + '</td>';
 
-                for (var i = 0; i < countUnit; i++) {
-                    isi += '<td>' + a.data[index].data[i] + '</td>';
-                };
+//                for (var i = 0; i < countUnit; i++) {
+//                    isi += '<td>' + a.data[index].data[i] + '</td>';
+//                };
 
-                $("table").append(buka + isi + tutup);
-            }
-        })
-        .always(function () {
-            //$("table").show();
-            //$("#collapseOne").collapse('hide')
-            ////tablee = $("table").DataTable({
-            //    dom: "Brtip",
-            //    buttons: [
-            //    {
-            //        extend: "excelHtml5",
-            //        text: "Excel",
-            //        filename: "Reporttt",
-            //    }
-            //    ]
-            //})
-        })
-}
+//                $("table").append(buka + isi + tutup);
+//            }
+//        })
+//        .always(function () {
+//            //$("table").show();
+//            //$("#collapseOne").collapse('hide')
+//            ////tablee = $("table").DataTable({
+//            //    dom: "Brtip",
+//            //    buttons: [
+//            //    {
+//            //        extend: "excelHtml5",
+//            //        text: "Excel",
+//            //        filename: "Reporttt",
+//            //    }
+//            //    ]
+//            //})
+//        })
+//}
 
 $(document).ready(function () {
     $("#formGenerate").submit(function (e) {
@@ -79,52 +79,49 @@ $(document).ready(function () {
         var tgl1 = tg1.getFullYear() + "-" + (((tg1.getMonth() + 1) < 10) ? ("0" + (tg1.getMonth() + 1)) : (tg1.getMonth() + 1)) + "-" + ((tg1.getDate() < 10) ? ("0" + tg1.getDate()) : tg1.getDate());
         var tg2 = new Date($("#f_generateReport input[name='tgTo']").val());
         var tgl2 = tg2.getFullYear() + "-" + (((tg2.getMonth() + 1) < 10) ? ("0" + (tg2.getMonth() + 1)) : (tg2.getMonth() + 1)) + "-" + ((tg2.getDate() < 10) ? ("0" + tg2.getDate()) : tg2.getDate());
-
+        //$('#tbUnit').DataTable().destroy();
         $.ajax({
             "url": "api/report/" + tgl1 + "/" + tgl2 + "/" + tipe + "/" + ves,
             "success": function (json) {
-
-                $("#reportUnitTable").empty();
-                $("#reportUnitTable").append("<table id='rUnit' class='table display table-bordered' style='width:100%'></table>");
-                //console.log(json.data[3].datax[1]);
-                var t_head = $("#reportUnitTable").children();
-                var tableHeaders, isi;
-                //$('#here_table').append('<table></table>');
-                $.each(json.columns, function (i, val) {
-                    //console.log(val);
-                    tableHeaders += "<th>" + val + "</th>";
+                var kolom = [];
+                $.each(json.columns, function(i, value){
+                    //var obj = { sTitle: value };
+                    var obj = { title: value };
+                    kolom.push(obj);
                 });
-                t_head.append("<thead><tr>" + tableHeaders + "</tr></thead>");
-                
-                
-                //$("#reportUnitTable").append('<table id="rUnit" class="table display table-bordered" style="width:100%" ><thead><tr>' + tableHeaders + '</tr></thead></table>');
 
-                $.each(json.data, function(i,val){
-                    //console.log(val.tg);
-                    isi = '<td>' + val.tg + '</td><td>' + val.ves + '</td>';
+                var dd = [];
+                var aa = [];
+                $.each(json.data, function (i, val) {
+                    aa.push(val.tg);
+                    aa.push(val.ves);
+                    //aa.push(val.datax)
+                    //console.log(val.datax);
                     for (var k = 0; k < val.datax.length; k++) {
                         //console.log(val.datax[k]);
-                        isi += '<td>' + val.datax[k] + '</td>';
+                        aa.push(val.datax[k]);
                     }
-                    //$("#rUnit").append('<tr>' + isi + '</tr>');
-                    t_head.append("<tbody><tr>" + isi + "</tr></tbody>");
+                    dd.push(aa);
+                    aa = [];
                 });
-               
 
-
-                //var d = $('#rUnit').dataTable({
-                var d = t_head.dataTable({
+                //console.log(dd);
+                $('#tbUnit').DataTable({
                     dom: "Brtip",
-                        buttons: [
-                            {
-                                extend: "excelHtml5",
-                                text: "Excel",
-                                filename: "Reporttt",
-                            }
-                        ]
+                    destroy: true,
+                    data: dd,
+                    columns: kolom,
+                    buttons: [
+                        {
+                            extend: "excelHtml5",
+                            text: "Excel",
+                            filename: "Report_" + tgl1 + "_" + tgl2
+                        }
+                    ]
                 });
             },
             "dataType": "json"
         });
+
     });
 })
