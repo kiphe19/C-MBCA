8a10ec4d55ab57c1a27f3a490cc36cbb95c657db
jono <joejond@gmail.com>
2016-04-19 11:34:17 +0700

    buat hitung hitungan

--- MBCA/Controllers/ExportController.cs
+++ MBCA/Controllers/ExportController.cs
@@ -44,86 +44,6 @@ namespace chevron.Controllers
                 colidx++;
             }
         }
-        //public void r_dailyUnit(string tg1,string tg2, int v)
-        //{
-        //    DateTime dateFrom = DateTime.ParseExact(tg1, "yyyyMMdd", CultureInfo.InvariantCulture);
-        //    DateTime dateTo = DateTime.ParseExact(tg2,"yyyyMMdd",CultureInfo.InvariantCulture);
-        //    TimeSpan ambilTanggal = dateTo - dateFrom;
-        //    var vesselname = "";
-        //    con.select("vessel_table", "name", string.Format("id={0}", v));
-        //    while (con.result.Read())
-        //    {
-        //        vesselname = con.result["name"].ToString();
-        //    }
-
-
-        //    var periode = dateFrom.ToString("dd MMM") + " - " + dateTo.ToString("dd MMM yy");
-        //    var period = dateFrom.ToString("dd MMM") + "_" + dateTo.ToString("dd MMM yy");
-
-        //    JArray header = new JArray();
-        //    JArray unitid = new JArray();
-        //    JArray unitnama = new JArray();
-        //    var dt = new DataTable();
-        //    header.Add("Date");
-        //    //header.Add("Vesse")
-        //    var cr = string.Format("tgl >= '{0}' and tgl <= '{1}' and id_vessel = {2}", dateFrom.ToString("yyyy-MM-dd"), dateTo.ToString("yyyy-MM-dd"), v);
-        //    con.select("report_daily join unit_table on unit_table.id = report_daily.id_unit", "distinct(id_unit) id_unit,unit_table.name nama", cr);
-        //    while (con.result.Read())
-        //    {
-        //        unitid.Add(con.result["id_unit"]);
-        //        unitnama.Add(con.result["nama"]);
-        //        header.Add(con.result["nama"]);
-
-
-        //    }
-
-        //    dt.Columns.Add("date");
-
-        //    for (var i = 0; i <= ambilTanggal.TotalDays; i++)
-        //    {
-        //        //dt.NewRow();
-        //        var baris = dt.NewRow();
-
-        //        baris["date"] = dateFrom.AddDays(i).ToString("yyyy-MM-dd");
-
-        //        //foreach (var u in unitid)
-        //        //{
-
-        //        //}
-        //    }
-
-
-
-
-        //    using (ExcelPackage pkg = new ExcelPackage())
-        //    {
-        //        ExcelWorksheet ws = pkg.Workbook.Worksheets.Add("coba");
-        //        //buat gambar
-        //        this.logoGb(ws);
-                
-        //        ws.Cells[7, 1].Value = "Month :"; ws.Cells[7, 3].Value = periode; 
-        //        ws.Cells[8, 1].Value = "Boat Name :"; ws.Cells[8, 3].Value = vesselname;
-        //        ws.Cells[9, 1].Value = "Boat Owner :";
-        //        using (ExcelRange cel = ws.Cells["A7:D9"])
-        //        {
-        //            cel.Style.Font.SetFromFont(new Font("Arial Narrow", 11, FontStyle.Bold));
-        //        }
-
-
-        //            //buat header judul
-        //        this.headerTbl(header, ws);
-
-        //        // isi tanggal
-        //        var mulai = 11;
-
-
-
-        //        //create excel
-        //        this.createxls(pkg, "daily_" + period);
-
-        //    }
-        //    //return RedirectToAction("Index");
-        //}
 
         private void createxls (ExcelPackage pkg, string filenama)
         {
@@ -140,63 +60,63 @@ namespace chevron.Controllers
         }
 
 
+        //public ActionResult report(string tg1, string tg2, int v)
+        //{
+        //    DataTable dt = new DataTable();
+        //    DateTime dateFrom = DateTime.ParseExact(tg1, "yyyyMMdd", CultureInfo.InvariantCulture);
+        //    DateTime dateTo = DateTime.ParseExact(tg2, "yyyyMMdd", CultureInfo.InvariantCulture);
+        //    TimeSpan ambilTanggal = dateTo - dateFrom;
 
-        public ActionResult report(string tg1, string tg2, int v)
-        {
-            DataTable dt = new DataTable();
-            DateTime dateFrom = DateTime.ParseExact(tg1, "yyyyMMdd", CultureInfo.InvariantCulture);
-            DateTime dateTo = DateTime.ParseExact(tg2, "yyyyMMdd", CultureInfo.InvariantCulture);
-            TimeSpan ambilTanggal = dateTo - dateFrom;
+        //    JArray unitid = new JArray();
 
-            JArray unitid = new JArray();
+        //    dt.Columns.Add("Date");
+        //    var cr = string.Format("tgl >= '{0}' and tgl <= '{1}' and id_vessel = {2}", dateFrom.ToString("yyyy-MM-dd"), dateTo.ToString("yyyy-MM-dd"), v);
+        //    con.select("report_daily join unit_table on unit_table.id = report_daily.id_unit", "distinct(id_unit) id_unit,unit_table.name nama", cr);
+        //    while (con.result.Read())
+        //    {
+        //        dt.Columns.Add(con.result["nama"].ToString());
+        //        unitid.Add(con.result["id_unit"]);
+        //    }
+        //    dt.Columns.Add("Totalx",typeof(float));
 
-            dt.Columns.Add("Date");
-            var cr = string.Format("tgl >= '{0}' and tgl <= '{1}' and id_vessel = {2}", dateFrom.ToString("yyyy-MM-dd"), dateTo.ToString("yyyy-MM-dd"), v);
-            con.select("report_daily join unit_table on unit_table.id = report_daily.id_unit", "distinct(id_unit) id_unit,unit_table.name nama", cr);
-            while (con.result.Read())
-            {
-                dt.Columns.Add(con.result["nama"].ToString());
-                unitid.Add(con.result["id_unit"]);
-            }
-            dt.Columns.Add("Totalx",typeof(float));
+        //    for (var i = 0; i <= ambilTanggal.TotalDays; i++)
+        //    {
+        //        DataRow brs = dt.NewRow();
 
-            for (var i = 0; i <= ambilTanggal.TotalDays; i++)
-            {
-                DataRow brs = dt.NewRow();
+        //        brs[0] = dateFrom.AddDays(i).ToString("yyyy-MM-dd");
+        //        int k = 1;
+        //        decimal tot_row = 0;
+        //        foreach(var id in unitid)
+        //        {
+        //            var ww = string.Format("select round(fuel_litre,3) fuel_litre,round(fuel_price,3) fuel_price,fuel_curr,round(charter_price,3) charter_price,round(mob_price,3) mob_price,charter_curr from report_daily where tgl = '{0}' and id_unit = {1} and id_vessel = {2};", dateFrom.AddDays(i).ToString("yyyy-MM-dd"), id, v);
+        //            con.query(ww);
+        //            con.result.Read();
+        //            if (con.result.HasRows)
+        //                brs[k] = con.result["fuel_litre"];
 
-                brs[0] = dateFrom.AddDays(i).ToString("yyyy-MM-dd");
-                int k = 1;
-                decimal tot_row = 0;
-                foreach(var id in unitid)
-                {
-                    var ww = string.Format("select round(fuel_litre,3) fuel_litre,round(fuel_price,3) fuel_price,fuel_curr,round(charter_price,3) charter_price,round(mob_price,3) mob_price,charter_curr from report_daily where tgl = '{0}' and id_unit = {1} and id_vessel = {2};", dateFrom.AddDays(i).ToString("yyyy-MM-dd"), id, v);
-                    con.query(ww);
-                    con.result.Read();
-                    if (con.result.HasRows)
-                        brs[k] = con.result["fuel_litre"];
+        //            else
+        //                brs[k] = 0;
 
-                    else
-                        brs[k] = 0;
+        //            tot_row += Convert.ToDecimal(brs[k]);
 
-                    tot_row += Convert.ToDecimal(brs[k]);
+        //            k++;
+        //        }
+        //        brs[k] = tot_row;
+        //        dt.Rows.Add(brs);
+        //    }
 
-                    k++;
-                }
-                brs[k] = tot_row;
-                dt.Rows.Add(brs);
-            }
+        //    var json = JsonConvert.SerializeObject(dt);
+        //    //return Json(new { nama="jono"}, JsonRequestBehavior.AllowGet);
+        //    return Json(json, JsonRequestBehavior.AllowGet);
+        //}
 
-            var json = JsonConvert.SerializeObject(dt);
-            //return Json(new { nama="jono"}, JsonRequestBehavior.AllowGet);
-            return Json(json, JsonRequestBehavior.AllowGet);
-        }
-
-        public ActionResult laporan(string tg1, string tg2, int v, string t)
+        public string laporan(string tg1, string tg2, int v, string t)
         {
             DataTable dt = DailyReport(tg1, tg2, v,t);
 
             var json = JsonConvert.SerializeObject(dt);
-            return Json(json,JsonRequestBehavior.AllowGet);
+            //return Json(json,JsonRequestBehavior.AllowGet);
+            return json;
 
 
         }
@@ -220,7 +140,7 @@ namespace chevron.Controllers
                 unitid.Add(con.result["id_unit"]);
             }
             //dt.Columns.Add("Total", typeof(float));
-            dt.Columns.Add("Total");
+            //dt.Columns.Add("Total");
 
             for (var i = 0; i <= ambilTanggal.TotalDays; i++)
             {
@@ -228,14 +148,13 @@ namespace chevron.Controllers
                 
                 brs[0] = dateFrom.AddDays(i).ToString("yyyy-MM-dd");
                 int k = 1;
-                decimal tot_row = 0;
+                //decimal tot_row = 0;
                 foreach (var id in unitid)
                 {
                     var ww = string.Format("select round(fuel_litre,3) fuel_litre,round(fuel_price,3) fuel_price,fuel_curr,round(charter_price,3) charter_price,round(mob_price,3) mob_price,charter_curr from report_daily where tgl = '{0}' and id_unit = {1} and id_vessel = {2};", dateFrom.AddDays(i).ToString("yyyy-MM-dd"), id, v);
                     con.query(ww);
                     con.result.Read();
                     if (con.result.HasRows)
-                        //brs[k] = con.result["fuel_litre"];
                         switch (t)
                         {
                             case "fl":
@@ -256,14 +175,12 @@ namespace chevron.Controllers
                             default:
                                 break;
                         }
-
-                        //brs[k] = con.result["charter_price"];
                     else
                         brs[k] = 0;
-                    tot_row += Convert.ToDecimal(brs[k]);
+                    //tot_row += Convert.ToDecimal(brs[k]);
                     k++;
                 }
-                brs[k] = tot_row;
+                //brs[k] = tot_row;
                 dt.Rows.Add(brs);
             }
             return dt;
@@ -316,15 +233,15 @@ namespace chevron.Controllers
                     //Setting the background color of header cells to Gray
                     var fill = cell.Style.Fill;
                     fill.PatternType = ExcelFillStyle.Solid;
-                    fill.BackgroundColor.SetColor(Color.Gray);
+                    fill.BackgroundColor.SetColor(Color.Aquamarine);
 
 
                     //Setting Top/left,right/bottom borders.
-                    var border = cell.Style.Border;
-                    border.Bottom.Style =
-                        border.Top.Style =
-                        border.Left.Style =
-                        border.Right.Style = ExcelBorderStyle.Medium;
+                    var bd1 = cell.Style.Border;
+                    bd1.Bottom.Style =
+                        bd1.Top.Style =
+                        bd1.Left.Style =
+                        bd1.Right.Style = ExcelBorderStyle.Medium;
 
                     //Setting Value in cell
                     cell.Value = dc.ColumnName;
@@ -351,13 +268,14 @@ namespace chevron.Controllers
 
                         else cell.Value = dr[dc.ColumnName];
                         //Setting borders of cell
-                        var border = cell.Style.Border;
-                        border.Left.Style =
-                            border.Right.Style = ExcelBorderStyle.Thin;
+                        var bd2 = cell.Style.Border;
+                        bd2.Left.Style =
+                            bd2.Right.Style = ExcelBorderStyle.Thin;
                         colIndex++;
                     }
                 }
 
+                //hitung total per colom
                 colIndex = 1;
                 foreach (DataColumn dc in dt.Columns) //Creating Formula Sum
                 {
@@ -374,12 +292,92 @@ namespace chevron.Controllers
                                     ")";
                     }
 
-
+                    var bd = cell.Style.Border;
+                        bd.Top.Style = ExcelBorderStyle.Medium;
+                        bd.Left.Style = ExcelBorderStyle.Thin;
+                        bd.Right.Style = ExcelBorderStyle.Thin;
+                        bd.Bottom.Style = ExcelBorderStyle.Double;
                     //Setting Background fill color to Gray
                     cell.Style.Fill.PatternType = ExcelFillStyle.Solid;
-                    cell.Style.Fill.BackgroundColor.SetColor(Color.Gray);
+                    cell.Style.Fill.BackgroundColor.SetColor(Color.Aquamarine);
                     colIndex++;
                 }
+
+                //colIndex = 1;
+                //decimal total_r = 0;
+                string a="";
+                switch (t)
+                {
+                    case "fl":
+                        a = "Fuel";
+                        break;
+                    case "fc":
+                        a = "Fuel Cost";
+                        break;
+                    case "ch":
+                        a = "Charter";
+                        break;
+                    case "mb":
+                        a = "Mob/Demob";
+                        break;
+                    case "ap":
+                        a = "All Cost";
+                        break;
+                    default: break;                            
+                }
+
+
+                int row_total = rowIndex + 4;
+                int col_total = colIndex - 2;
+
+                ws.Cells[row_total, col_total].Value = "Total "+a;
+                var tt = ws.Cells[row_total, col_total+1];
+
+                var border = tt.Style.Border;
+                border.Left.Style = ExcelBorderStyle.Medium;
+                border.Right.Style = ExcelBorderStyle.Medium;
+                border.Bottom.Style = ExcelBorderStyle.Double;
+                tt.Style.Fill.PatternType = ExcelFillStyle.Solid;
+                tt.Style.Fill.BackgroundColor.SetColor(Color.Aquamarine);
+
+
+                tt.Formula = "SUM("+
+                            //ws.Cells[rowIndex + 1,2].Address+":"+
+                            ws.Cells[row_total-3, 2].Address+":"+
+                            ws.Cells[row_total - 3, col_total + 1].Address+")";
+
+                colIndex = 1;
+                foreach (DataColumn dc in dt.Columns)
+                {
+                    var cell_p = ws.Cells[rowIndex + 2, colIndex];
+                    //if (colIndex == 1) cell_p.Value = "";
+                    if (colIndex > 1) 
+                    //else
+                    {
+                        cell_p.Style.Numberformat.Format = "0.00%";
+                        cell_p.Formula = "+" + ws.Cells[rowIndex+1, colIndex].Address + "/" + tt.Address;
+                        //cell_p.Value = "R" + (rowIndex + 2) + "C" + colIndex;
+                    }
+                    var bd = cell_p.Style.Border;
+                    bd.Top.Style = ExcelBorderStyle.Medium;
+                    bd.Left.Style = ExcelBorderStyle.Thin;
+                    bd.Right.Style = ExcelBorderStyle.Thin;
+                    bd.Bottom.Style = ExcelBorderStyle.Medium;
+                    colIndex++;
+                }
+                int row_p = rowIndex + 3;
+                var prs = ws.Cells[row_p, colIndex-1];
+
+                var grs = prs.Style.Border;
+                grs.Left.Style = ExcelBorderStyle.Medium;
+                grs.Right.Style = ExcelBorderStyle.Medium;
+                grs.Bottom.Style = ExcelBorderStyle.Medium;
+
+                prs.Style.Numberformat.Format = "0.00%";
+                prs.Formula = "SUM("+
+                                ws.Cells[row_p-1,1].Address+":"+
+                                ws.Cells[row_p - 1,colIndex-1].Address
+                            +")";
 
                 this.createxls(p, "daily_" + period);
             }
